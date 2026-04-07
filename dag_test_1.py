4ce306c22c2ddf5681bdf52bf51655fe2a702853
Gudsfile <gudsfile@protonmail.com>
2026-04-07 14:36:26 +0000

    sleep

--- dag_test_1.py
+++ dag_test_1.py
@@ -2,6 +2,7 @@ import datetime
 
 from airflow.sdk import DAG
 from airflow.providers.standard.operators.empty import EmptyOperator
+from airflow.providers.standard.operators.bash import BashOperator
 
 from assets import ASSET_TEST_1_FIRST_TASK
 
@@ -11,6 +12,6 @@ with DAG(
     start_date=datetime.datetime(2021, 1, 1),
     schedule="@daily",
 ):
-    first_task = EmptyOperator(task_id="first_task", outlets=ASSET_TEST_1_FIRST_TASK)
-    second_task = EmptyOperator(task_id="second_task")
+    first_task = BashOperator(task_id="first_task", bash_command="sleep 10; echo task_1", outlets=ASSET_TEST_1_FIRST_TASK)
+    second_task = BashOperator(task_id="second_task", bash_command="sleep 5; echo task_2")
     first_task >> second_task
