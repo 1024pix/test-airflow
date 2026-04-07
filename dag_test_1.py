91b48667121d398355f02dc512b04a1eec0fbf05
Gudsfile <gudsfile@protonmail.com>
2026-04-07 15:11:58 +0000

    add third_task

--- dag_test_1.py
+++ dag_test_1.py
@@ -12,6 +12,8 @@ with DAG(
     start_date=datetime.datetime(2021, 1, 1),
     schedule="@daily",
 ):
-    first_task = BashOperator(task_id="first_task", bash_command="sleep 10; echo task_1", outlets=ASSET_TEST_1_FIRST_TASK)
-    second_task = BashOperator(task_id="second_task", bash_command="sleep 5; echo task_2")
-    first_task >> second_task
+    first_task = BashOperator(task_id="first_task", bash_command="echo task_1", outlets=ASSET_TEST_1_FIRST_TASK)
+    second_task = BashOperator(task_id="second_task", bash_command="echo task_2")
+    third_task = EmptyOperator(task_id="third_task")
+
+    first_task >> second_task >> third_task
