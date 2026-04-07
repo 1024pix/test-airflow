fdbec4467b9912550ed3cc309bf64fb5e66f3702
Gudsfile <gudsfile@protonmail.com>
2026-04-07 13:50:06 +0000

    fix dags

--- dag_test_1.py
+++ dag_test_1.py
@@ -1,7 +1,7 @@
- import datetime
+import datetime
 
- from airflow.sdk import DAG
- from airflow.providers.standard.operators.empty import EmptyOperator
+from airflow.sdk import DAG
+from airflow.providers.standard.operators.empty import EmptyOperator
 
 from assets import ASSET_TEST_1_FIRST_TASK
 
@@ -10,6 +10,6 @@ with DAG(
     start_date=datetime.datetime(2021, 1, 1),
     schedule="@daily",
 ):
-   first_task = EmptyOperator(task_id="first_task", outlets=ASSET_TEST_1_FIRST_TASK)
-   second_task = EmptyOperator(task_id="second_task")
-   first_task >> second_task
+    first_task = EmptyOperator(task_id="first_task", outlets=ASSET_TEST_1_FIRST_TASK)
+    second_task = EmptyOperator(task_id="second_task")
+    first_task >> second_task
--- dag_test_2.py
+++ dag_test_2.py
@@ -1,7 +1,7 @@
- import datetime
+import datetime
 
- from airflow.sdk import DAG
- from airflow.providers.standard.operators.empty import EmptyOperator
+from airflow.sdk import DAG
+from airflow.providers.standard.operators.empty import EmptyOperator
 
 from assets import ASSET_TEST_1_FIRST_TASK
 
@@ -11,6 +11,6 @@ with DAG(
     start_date=datetime.datetime(2021, 1, 1),
     schedule=[ASSET_TEST_1_FIRST_TASK],
 ):
-   first_task = EmptyOperator(task_id="first_task")
-   second_task = EmptyOperator(task_id="second_task")
-   first_task >> second_task
+    first_task = EmptyOperator(task_id="first_task")
+    second_task = EmptyOperator(task_id="second_task")
+    first_task >> second_task
